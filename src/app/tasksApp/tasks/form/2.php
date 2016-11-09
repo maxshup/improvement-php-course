b92c9f5c8600a71aa86760d662ac4c55b3f53f41
Maxim <Max.shupenko@gmail.com>
2016-11-09 19:40:06 +0700

    seminar

---
+++ src/app/tasksApp/tasks/form/2.php
@@ -0,0 +1,24 @@
+<form action="register.php" method="post" name="r_form" >
+	Имя:
+	<br>
+	<input type="text" name="r_first_name" required=" ">
+	<br>
+	Фамилия:
+	<br>
+	<input type="text" name="r_last_name" required=" ">
+	<br>
+	Электронная почта:
+	<br>
+	<input type="text" name="r_email" required=" ">
+	<br>
+	Логин:
+	<br> 
+	<input type="text" name="r_login" required=" "> 
+	<br>
+	Пароль: 
+	<br>
+	<input type="password" name="r_password" required=" ">
+	<br>
+	<input type="submit" name="r_send" value="Зарегистрироваться!">
+</form>
+<>
